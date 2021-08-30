770fce74e71ad8c7179c8f0a8ffd4403d471ec94
SnubbyOWO <87741849+SnubbyOWO@users.noreply.github.com>
2021-08-30 14:51:58 -0400

    diff name

--- source/Discord.hx
+++ source/Discord.hx
@@ -12,7 +12,7 @@ class DiscordClient
 	{
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "557069829501091850", // change this to what ever the fuck you want lol
+			clientID: "881966599085654016", // change this to what ever the fuck you want lol
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
@@ -40,7 +40,7 @@ class DiscordClient
 			details: "In the Menus",
 			state: null,
 			largeImageKey: 'icon',
-			largeImageText: "fridaynightfunkin"
+			largeImageText: "friday night funkin' VS Sno"
 		});
 	}
 
